39bbb2788a810cd83f85fc870193b7ac0f7b9fbe
grbalmeida <g.r.almeida@live.com>
2023-10-28 10:35:56 -0300

    Ação de Login

--- src/contexto/SessaoUsuario.jsx
+++ src/contexto/SessaoUsuario.jsx
@@ -1,4 +1,5 @@
 import createContext, { useContext } from 'react';
+import axios from 'axios';
 
 const SessaoUsuarioContext = createContext({
     usuarioEstaLogado: false,
@@ -12,9 +13,19 @@ export const useSessaoUsuarioContext = () => {
 }
 
 export const SessaoUsuarioProvider = ({ children }) => {
+
+    const login = (email, senha) => {
+        axios
+            .post('http://localhost:8080/auth/login', {
+                email,
+                senha
+            })
+            .then(resposta => console.log(resposta))
+            .catch(erro => console.error(erro));
+    };
     
     const value = {
-
+        login
     };
     
     return (
